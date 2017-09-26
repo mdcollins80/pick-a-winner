9af72b54539b2a91c6d177fe0b93812aea4507a5
mdcollins80 <mdcollins80@gmail.com>
2017-09-26 10:38:26 -0400

    Add sort to onGetGamesSuccess
    -Sort incoming games array by games.id

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -7,11 +7,14 @@ const onGetGamesSuccess = function (data) {
   const sorted = data.games.sort(function (a, b) {
     return a.id - b.id
   })
+
+  // COMMENTED OUT THE HANDLEBARS CODE:
   // const showGamesHtml = showGamesTemplate(data)
   // $('.picks-card').append(showGamesHtml)
+
   $('.picks-card').append('<ul class="games-list"></ul>')
   sorted.forEach(function (game) {
-    const element = $('<li data-id="' + game.id + '"class="game-list-item">Week: ' + game.week_num + '  |  Kickoff: ' + game.time + '  |  Away ID: ' + game.away_team_id + '  |  Home ID: ' + game.home_team_id + '<button data-id="' + game.away_team_id + '" class="away-button">Away</button>  <button data-id="' + game.home_team_id + '" class="home-button">Home</button> </li>')
+    const element = $('<li data-id="' + game.id + '"class="game-list-item">Week: ' + game.week_num + '  |  Kickoff: ' + game.time + '  |  Away ID: ' + game.away_team_id + '  |  Home ID: ' + game.home_team_id + '<button type="submit" data-id="' + game.away_team_id + '" class="away-button">Away</button>  <button type="submit" data-id="' + game.home_team_id + '" class="home-button">Home</button></li>')
     $('.games-list').append(element)
   })
 }
